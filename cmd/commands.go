0f4b32cbb098954f2bb10a31bbea7f934f69b71b
Joshua Hyde <jrh3k5+github@gmail.com>
2023-08-06 14:51:29 -0500

    Move to jrh3k5 repo
    Move to jrh3k5 repo

--- cmd/commands.go
+++ cmd/commands.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jrudio/go-plex-client"
+	"github.com/jrh3k5/go-plex-client"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli"
 )
@@ -801,7 +801,7 @@ func stopPlayback(c *cli.Context) error {
 
 	sessionID := selectedSession.Session.ID
 
-	if err := plexConn.TerminateSession(sessionID, "stream stopped by github.com/jrudio/go-plex-client"); err != nil {
+	if err := plexConn.TerminateSession(sessionID, "stream stopped by github.com/jrh3k5/go-plex-client"); err != nil {
 		return cli.NewExitError(err, 1)
 	}
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/jrudio/go-plex-client
+module github.com/jrh3k5/go-plex-client
 
 go 1.12
 
